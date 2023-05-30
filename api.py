4b91a016d31946599f3170ee4910e90866ef50ca
Harsh Gandhi <gandhiharsh082@gmail.com>
2023-05-30 16:38:48 +0530

    add comments

--- api.py
+++ api.py
@@ -1,9 +1,17 @@
 import os
 import openai
+
+# Set OpenAI API key
 openai.api_key = "REPLACE_TO_API_KEY"
+
+# Define start and restart sequences for conversation
 start_sequence = "\nA:"
 restart_sequence = "\n\nQ: "
+
+# Initialize prompt
 prompt = ""
+
+# Function to call the OpenAI API
 def callAPI(prompt):
   response = openai.Completion.create(
       model="text-davinci-003",
@@ -14,9 +22,5 @@ def callAPI(prompt):
       frequency_penalty=0,
       presence_penalty=0,
       stop=["\n"]
-
-    )
+  )
   return response.choices[0].text
-
-
-
--- main.py
+++ main.py
@@ -1,27 +1,39 @@
 from flask import Flask, render_template, request
 import api
 app = Flask(__name__)
+# create  route
 @app.route("/")
+# Display the index page 
 def index():
     return render_template('index.html')
+# Get the question from the front end using the POST method   
 @app.route("/getAnswer" ,methods=['GET','POST'])
 def getAnswer():
     start_sequence = "\nA:"
+  # Verify the method 
     if(request.method == 'POST'):
+       # Verify api header content type 
         content_type = request.headers.get('Content-Type')
         if (content_type == 'application/json'):
+            #  Data is stored in a json variable from the frontend javascript API
             json = request.json
             # print(json['prompt'])
+            # The prompt is retrieved from the json file. An open AI prompt that sends an answer back
+            # prompt like Q: How many squigs are in a bonk?\nA:  
+            # The Q: stands for question, and the A: stands for Answer . 
             prompt=json['prompt']+start_sequence
             # print(prompt)
+            # openai api call 
             response = api.callAPI(prompt)
             json_response = {
                 "answer": response,
                 "prompt":prompt+response
             }
             # print(json_response)
+            # return an  object 
             return json_response
         else:
+            #return  content_type is not application/json 
             json_response = {
                 "answer": "Something went wrong",
             }
@@ -29,6 +41,7 @@ def getAnswer():
         # prompt = request.form.get('prompt')
 
     else:
+          #return  when request not POST 
         json_response = {
             "answer": "Something went wrong",
         }
